e082fe6a741bcbe9c6985c39ee29794780f0319b
Krystle Lau <krystlelau90@gmail.com>
2022-02-14 17:13:30 +0800

    api ready to take search value

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import Books from "./Books.js";
+import MyLib from "./MyLib.js";
+import Search from "./Search.js";
 import "./styles.css";
 
 const App = () => {
--- src/components/Books.js
+++ src/components/Books.js
@@ -9,10 +9,8 @@ const Books = () => {
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
 
-  console.log(search);
-
   //FIX: restrict data obtained from API. think of how to narrow down selection
-  let url = "https://openlibrary.org/search.json?author=bloom";
+  let url = "https://openlibrary.org/search.json?q=" + search.value;
   useEffect(() => {
     setLoading(true);
     fetch(url)
@@ -21,6 +19,9 @@ const Books = () => {
       .then(() => setLoading())
       .catch(setError); //.catch deals with rejected cases
   }, []);
+
+  console.log(search);
+  console.log(url);
 
   if (loading) {
     return <h1 style={{ textAlign: "center" }}>Loading...</h1>;
@@ -33,6 +34,7 @@ const Books = () => {
   if (!data) {
     return null;
   } else {
+    //if there is data to fetch, it is rendered here
     let booksarr = data.docs;
     const booksTable = booksarr.map((el, index) => {
       return (
@@ -46,6 +48,7 @@ const Books = () => {
     const searchHandler = (search) => {
       setSearch(search);
       if (search !== "") {
+        //booksList is a filter to search for the books
         const bookList = booksarr.filter((book) => {
           //FIX: find out how to filter for author anbd title
           return Object.values(book)
