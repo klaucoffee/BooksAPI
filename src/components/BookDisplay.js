1baabb12d5e9b28e7a7a26cd6cfe24d1d63e8c20
Krystle Lau <krystlelau90@gmail.com>
2022-02-16 17:26:57 +0800

    added bookdisplay output

--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-const BookDisplay = () => {
-  return <div>BookDisplay</div>;
+//deault needed because on first render, there are no images
+const DEFAULT_PLACEHOLDER_IMAGE =
+  "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg";
+
+//takes one prop at a time (book is the same as the prop in app.js)
+const BookDisplay = ({ book }) => {
+  let bookLCCN = book.lccn;
+
+  const coverurl =
+    "https://covers.openlibrary.org/b/lccn/" + bookLCCN + "-M.jpg";
+
+  return (
+    <div className="book">
+      <h2>{book.title}</h2>
+      <div>
+        <img
+          width="200"
+          alt={`The Book titled: ${book.title}`}
+          src={coverurl}
+        />
+      </div>
+      <p>({book.publish_year})</p>
+    </div>
+  );
 };
 
 export default BookDisplay;
