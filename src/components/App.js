351ee51dcb2e6fd2c5abea6df78034a4c87b788a
Krystle Lau <krystlelau90@gmail.com>
2022-02-16 17:21:39 +0800

    added useEffect and use States

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import BookDisplay from "./BookDisplay";
 
+const demo_url = `https://openlibrary.org/search.json?q="one+day+in+the+life"`;
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    fetch(demo_url)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        setBooks(jsonResponse.docs);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <BookDisplay />
