377fbc865bf040fc5d5e2ab0164498b73c89898d
Krystle Lau <krystlelau90@gmail.com>
2022-02-16 17:23:46 +0800

    added search - fetch url

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import BookDisplay from "./BookDisplay";
+import Search from "./Search";
 
 const demo_url = `https://openlibrary.org/search.json?q="one+day+in+the+life"`;
 
@@ -16,8 +17,38 @@ const App = () => {
       });
   }, []);
 
+  const search = (searchValue) => {
+    setLoading(true);
+    setErrorMessage(null);
+
+    let book_url = "https://openlibrary.org/search.json?q=" + searchValue;
+
+    fetch(book_url)
+      .then((response) => response.json())
+      .then((jsonResponse) => {
+        console.log("searchValue", searchValue);
+        console.log(book_url);
+        console.log(jsonResponse.numFound);
+        if (jsonResponse.numFound > 500) {
+          //too many responses
+          setErrorMessage(
+            "Too many responses - please narrow down search terms"
+          );
+          setLoading(false);
+        } else if (jsonResponse.numFound > 0) {
+          //if less than 500 but more than 0
+          setBooks(jsonResponse.docs);
+          setLoading(false);
+        } else {
+          setErrorMessage("Invalid Query");
+          setLoading(false);
+        }
+      });
+  };
+
   return (
     <div className="App">
+      <Search search={search} />
       <BookDisplay />
     </div>
   );
