522228ac4943de1996e3a743000043c105842005
Krystle Lau <krystlelau90@gmail.com>
2022-02-22 21:39:24 +0800

    removing homepage in package json

--- src/components/MyLib.js
+++ src/components/MyLib.js
@@ -1,5 +1,35 @@
 import React from "react";
 
+//store data in firebase based on what is clicked
+
+//"homepage": "https://klaucoffee.github.io/booksapi",
+
+//GOOGLE FIRESTORE / FIREBASE
+
+// Initialize Cloud Firestore through Firebase
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBd5yb3ihAUzfM8lNGNQbyvsZsC7TdAi5g",
+  authDomain: "booksapi-dace1.firebaseapp.com",
+  projectId: "booksapi-dace1",
+  storageBucket: "booksapi-dace1.appspot.com",
+  messagingSenderId: "88925925970",
+  appId: "1:88925925970:web:61803f0580201fdcef658b",
+  measurementId: "G-S9Q78V6W3E",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//use db to access database
+const db = getFirestore();
+
 const MyLib = () => {
   return <div>MyLib</div>;
 };
